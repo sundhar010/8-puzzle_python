2a16a97fe3bad8b4ba2d93a77cb85a97024647fa
sundhar010 <sundhar.i14@iiits.in>
2017-01-14 21:56:29 +0530

    solverd bfs and dfs

--- 8-puzzle_dfs.py
+++ 8-puzzle_dfs.py
@@ -37,19 +37,17 @@ def down(l):
 	l[i+3] = temp
 	return l
 
-def bfs(s,e):
+def dfs(s,e):
 	visited = []
 	q = [(s,[])] #[(vertex,path to this vertex [])]
 	while q:
-		current = tuple(q.pop(0))
+		current = tuple(q.pop())
 		current[1].append(current[0])
 		if(current[0] == e):
 			return current[1]
+			
 		if (left(list(current[0])) != -1) and (current[0] not in visited):
 			t = tuple((left(list(current[0])),list(current[1])))
-			q.append(t)
-		if (right(list(current[0])) != -1) and (current[0] not in visited):
-			t = tuple((right(list(current[0])),list(current[1])))
 			q.append(t)
 		if (up(list(current[0])) != -1) and (current[0] not in visited):
 			t = tuple((up(list(current[0])),list(current[1])))
@@ -57,12 +55,15 @@ def bfs(s,e):
 		if (down(list(current[0])) != -1) and (current[0] not in visited):
 			t = tuple((down(list(current[0])),list(current[1])))
 			q.append(t)
+		if (right(list(current[0])) != -1) and (current[0] not in visited):
+			t = tuple((right(list(current[0])),list(current[1])))
+			q.append(t)
 		visited.append(current)
 
 
 #start
-start = [1,0,3,
-		4,2,5,
+start = [0,1,2,
+		4,5,3,
 		7,8,6]
 
 #End
@@ -70,7 +71,7 @@ end = [1,2,3,
 	   4,5,6,
 	   7,8,0]
 
-path =  bfs(list(start),list(end))
+path =  dfs(list(start),list(end))
 
 for l in path:
 	print l[0:3]
