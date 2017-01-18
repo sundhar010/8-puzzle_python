648a711722037257410d0982dfb2e87d103397a2
sundhar010 <sundhar.i14@iiits.in>
2017-01-18 10:29:56 +0530

    UCS completed

--- 8-puzzle_ucs.py
+++ 8-puzzle_ucs.py
@@ -1,67 +1,121 @@
+from Queue import PriorityQueue
 
-def left(l):
+
+class MyPriorityQueue(PriorityQueue):
+    def __init__(self):
+        PriorityQueue.__init__(self)
+        self.counter = 0
+
+    def put(self, item, priority):
+        PriorityQueue.put(self, (priority, self.counter, item))
+        self.counter += 1
+
+    def get(self, *args, **kwargs):
+        _, _, item = PriorityQueue.get(self, *args, **kwargs)
+        return item
+
+
+def left(t):
+	l = list(t[0])
+	cost = t[2]
 	i = l.index(0)
 	if i  in [0,3,6]:
 		return -1
 	temp = l[i-1]
+	if temp in [1,2,3]:
+		cost += 1
+	if temp in [4,5,6]:
+		cost += 2
+	if temp in [7,8]:
+		cost += 3 
 	l[i-1] = l[i]
-	l[i] = temp 
-	return l
+	l[i] = temp
+	path = list(t[1])
+	return tuple((l,path,cost))
+ 	
 
-
-def right(l):
+def right(t):
+	l = list(t[0])
+	cost = t[2]
 	i = l.index(0)
 	if i  in [2,5,8]:
 		return -1
 	temp = l[i+1]
+	if temp in [1,2,3]:
+		cost += 1
+	if temp in [4,5,6]:
+		cost += 2
+	if temp in [7,8]:
+		cost += 3 
 	l[i+1] = l[i]
-	l[i] = temp 
-	return l
+	l[i] = temp
+	path = list(t[1])
+	return tuple((l,path,cost))
 
-
-def up(l):
+def up(t):
+	l = list(t[0])
+	cost = t[2]
 	i = l.index(0)
-	if i in [0,1,2]:
+	if i  in [0,1,2]:
 		return -1
-	temp = l[i]
-	l[i] = l[i-3]
-	l[i-3] = temp
-	return l
+	temp = l[i-3]
+	if temp in [1,2,3]:
+		cost += 1
+	if temp in [4,5,6]:
+		cost += 2
+	if temp in [7,8]:
+		cost += 3 
+	l[i-3] = l[i]
+	l[i] = temp
+	path = list(t[1])
+	return tuple((l,path,cost))
 
-def down(l):
+def down(t):
+	l = list(t[0])
+	cost = t[2]
 	i = l.index(0)
-	if i in [6,7,8]:
+	if i  in [6,7,8]:
 		return -1
-	temp = l[i]
-	l[i] = l[i+3]
-	l[i+3] = temp
-	return l
+	temp = l[i+3]
+	if temp in [1,2,3]:
+		cost += 1
+	if temp in [4,5,6]:
+		cost += 2
+	if temp in [7,8]:
+		cost += 3 
+	l[i+3] = l[i]
+	l[i] = temp
+	path = list(t[1])
+	return tuple((l,path,cost))
 
 def bfs(s,e):
 	visited = []
-	q = [(s,[])] #[(vertex,path to this vertex [])]
+	start = (s,[],0) #(vertex,path to this vertex [])
+	q = MyPriorityQueue()
+	q.put(tuple(start),start[2])
 	while q:
-		current = tuple(q.pop(0))
+		current = tuple(q.get())
 		current[1].append(current[0])
 		if(current[0] == e):
 			return current[1]
-		if (left(list(current[0])) != -1) and (current[0] not in visited):
-			t = tuple((left(list(current[0])),list(current[1])))
-			q.append(t)
-		if (right(list(current[0])) != -1) and (current[0] not in visited):
-			t = tuple((right(list(current[0])),list(current[1])))
-			q.append(t)
-		if (up(list(current[0])) != -1) and (current[0] not in visited):
-			t = tuple((up(list(current[0])),list(current[1])))
-			q.append(t)
-		if (down(list(current[0])) != -1) and (current[0] not in visited):
-			t = tuple((down(list(current[0])),list(current[1])))
-			q.append(t)
-		if current[0] not in visited :
+		if (left(list(current)) != -1) and (current[0] not in visited):
+			t = tuple(left(tuple(current)))
+			q.put(t,t[2])
+		if (right(list(current)) != -1) and (current[0] not in visited):
+			t = tuple(right(list(current)))
+			q.put(t,t[2])
+		if (up(list(current)) != -1) and (current[0] not in visited):
+			t = tuple(up(list(current)))
+			q.put(t,t[2])
+		if (down(list(current)) != -1) and (current[0] not in visited):
+			t = tuple(down(list(current)))
+			q.put(t,t[2])
+		if current[0] not in visited:
 			visited.append(current[0])
 
+
 #start
-start = [1,0,3,
+start = [0,1,3,
 		4,2,5,
 		7,8,6]
 
